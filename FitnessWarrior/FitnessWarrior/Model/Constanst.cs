def6ca93c977cd08ccc5206fb11487c5cb4f5179
Quang Lu <nqlu@deakin.edu.au>
2018-09-09 23:22:04 +1000

    Add .gitignore and .gitattributes.

---
+++ FitnessWarrior/FitnessWarrior/Model/Constanst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessWarrior.Model
+{
+    public class Constanst
+    {
+        public static bool isDev = true;
+    }
+}
